1c6475a8f0d1e459f7b9746c1fe57cb3f7494641
Harald Ruda <h.ruda@neu.edu>
2020-03-22 16:41:42 -0400

    read from URL, more lines, choose country/state

--- covidplot.py
+++ covidplot.py
@@ -1,30 +1,53 @@
 
+# IMPORTS
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from matplotlib.backends.backend_pdf import PdfPages
+# from matplotlib.backends.backend_pdf import PdfPages   # only neede for multipage PDFs, plt.close() for each page
 
+# CONSTANTS
 
+DataLocation = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
 
-data_location = 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series'
-confirmed = 'time_series_19-covid-Confirmed.csv'
-recovered = 'time_series_19-covid-Recovered.csv'
-deaths = 'time_series_19-covid-Deaths.csv'
+ConfirmedFile = 'time_series_19-covid-Confirmed.csv'
+RecoveredFile = 'time_series_19-covid-Recovered.csv'
+DeathsFile = 'time_series_19-covid-Deaths.csv'
 
+# DATA
 
-plt.clf()
+row = 101  
+KnownLocations = {17: 'Sweden', 101: 'Massachusetts'}
+RowLocation = KnownLocations[row]
 
-data = pd.read_csv('time_series_19-covid-Confirmed.csv')
+confirmed_data = pd.read_csv(DataLocation + ConfirmedFile)
+recovered_data = pd.read_csv(DataLocation + RecoveredFile)
+deaths_data = pd.read_csv(DataLocation + DeathsFile)
 
-pdat = data.loc[101][4:]
-lll = pdat.tolist()
+confirmed = np.array(confirmed_data.loc[row][4:].tolist())
+cumul_confirmed = np.transpose(confirmed[1:])
+daily_confirmed = np.transpose(confirmed[1:] - confirmed[:-1])
 
-lll = np.array(lll)
-sss = [ lll[1:], lll[1:] - lll[:-1] ]
+recovered = np.array(recovered_data.loc[row][4:].tolist())
+cumul_recovered = np.transpose(recovered[1:])
 
-plt.plot(np.transpose(sss))
+deaths = np.array(deaths_data.loc[row][4:].tolist())
+cumul_deaths = np.transpose(deaths[1:])
 
-plt.savefig('file.pdf')
+# PLOT
+
+fig, ax = plt.subplots()
+
+ax.plot(cumul_confirmed, label='Cumulative Cases')
+ax.plot(daily_confirmed, label='Daily New Cases')
+ax.plot(cumul_recovered, label='Cumulative Recovered')
+ax.plot(cumul_deaths, label='Cumulative Deaths')
+
+ax.grid()
+ax.legend(title='Where:')
+ax.set(ylabel='Number of Cases', xlabel='Day Number', title=RowLocation + ' COVID-19 Cases')
+
+fig.savefig('file.pdf')
+plt.show()
 
