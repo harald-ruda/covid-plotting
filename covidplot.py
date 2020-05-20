f3b9f993c00a7672426b9156dc2250aeef308b4b
Harald Ruda <h.ruda@neu.edu>
2020-05-20 02:40:05 -0400

    reorganized country lookups

--- covidplot.py
+++ covidplot.py
@@ -29,6 +29,17 @@ PDF = 'pdf'
 XKCD = 'xkcd'
 INFO = 'info'
 
+NAME = 'name'
+ABBREVIATION = 'abbreviation'
+
+STATE = 'state'
+DEATHS = 'deaths'
+CASES = 'cases'
+DATE = 'date'
+
+COUNTRY_REGION = 'Country/Region'
+PROVINCE_STATE = 'Province/State'
+
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # DATA SOURCES - always get the latest available data
@@ -36,11 +47,14 @@ INFO = 'info'
 
 
 # NYT DATA -- good data for US states
-DataLocation1 = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/'
+
+StateDataLocation = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/'
 StateFile = 'us-states.csv'
 
+
 # Johns Hopkins DATA -- good data for most countries
-DataLocation2 = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
+
+GlobalDataLocation = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
 ConfirmedFile = 'time_series_covid19_confirmed_global.csv'
 DeathsFile = 'time_series_covid19_deaths_global.csv'
 
@@ -50,17 +64,19 @@ DeathsFile = 'time_series_covid19_deaths_global.csv'
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
+# common name -> name in data-file
+
 LocationExceptions = { 'Russia': 'Russian Federation', 
-                       'Korea': 'Korea, Republic of',
-                       'SKorea': 'Korea, Republic of',
-                       'SouthKorea': 'Korea, Republic of',
-                       'South-Korea': 'Korea, Republic of',
-                       'South Korea': 'Korea, Republic of',
+                       'Korea': 'Korea, South',
+                       'SKorea': 'Korea, South',
+                       'SouthKorea': 'Korea, South',
+                       'South-Korea': 'Korea, South',
+                       'South Korea': 'Korea, South',
                        'Iran': 'Iran, Islamic Republic of',
                      }
 
-NoDataExceptions = [ "Korea, Democratic People's Republic of", 
-                   ]
+
+# official country name -> name in data-file
 
 CountryExceptions = { 'United States of America': 'US', 
                       'Russian Federation': 'Russia',
@@ -70,6 +86,10 @@ CountryExceptions = { 'United States of America': 'US',
                     }
 
 
+PlotExceptions = { 'Korea, South': 'South Korea',
+                 }
+
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # DATA
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -77,101 +97,116 @@ CountryExceptions = { 'United States of America': 'US',
 
 def get_data(parameters):
     """
+    Look up data for location.
+    Location can be US states: MA, Ohio, NY, New Jersey, etc. abreviation or full name
+    Location can also be country or province, or some common abbreviation
+    US states get precedence for abreviations, i.e. CA is California, not Canada
     """
     global States, Countries
 
     location = parameters[LOCATION]
 
-    # confirmed_data = pd.read_csv(DataLocation2 + ConfirmedFile)
-    # deaths_data = pd.read_csv(DataLocation2 + DeathsFile)
-    # print("Please choose from: ", confirmed_data['Country/Region'].tolist())
-
-    if location in LocationExceptions.keys():
-        location = LocationExceptions[location]
-
     if location in States:   # US States
 
         state = States[location]
-        location = state['name']
-        alternatives = [ state['abbreviation'] ]
+        location = state[NAME]
+        alternatives = [state[ABBREVIATION]]
 
-        df = pd.read_csv(DataLocation1 + StateFile)
-        rows = df['state'] == location
+        df = pd.read_csv(StateDataLocation + StateFile)
+        rows = df[STATE] == location
 
-        deaths = df[rows]['deaths'].tolist()
-        cases = df[rows]['cases'].tolist()
-        dates = df[rows]['date'].tolist()
+        cumul_deaths = df[rows][DEATHS].tolist()
+        cumul_cases = df[rows][CASES].tolist()
+        dates = df[rows][DATE].tolist()
         lastday = dates[-1]
 
-        xvalues = dates
-        cumul_confirmed = cases
-        cumul_deaths = deaths
+        daily_cases = [0] + list(np.array(cumul_cases)[1:] - np.array(cumul_cases)[:-1])
+        daily_deaths = [0] + list(np.array(cumul_deaths)[1:] - np.array(cumul_deaths)[:-1])
+        xvalues = [x for x in range(len(daily_deaths))]
 
-        daily_confirmed = (np.array(cases)[1:] - np.array(cases)[:-1])
-        daily_confirmed = [0] + list(daily_confirmed)
+    else:   # Country or Province or Region outside of US
 
-        daily_deaths = (np.array(deaths)[1:] - np.array(deaths)[:-1])
-        daily_deaths = [0] + list(daily_deaths)
+        global_cases = pd.read_csv(GlobalDataLocation + ConfirmedFile)
+        global_deaths = pd.read_csv(GlobalDataLocation + DeathsFile)
 
-        xvalues = [x for x in range(len(daily_confirmed))]
+        country_list = global_deaths[COUNTRY_REGION].tolist()
+        province_list = global_deaths[PROVINCE_STATE].tolist()
 
-    elif location in Countries:   # Country
+        alternatives = []
 
-        country = Countries.get(location)
-        location = country.name
-        alternatives = [ country.alpha2, country.alpha3 ]   # numeric, apolitical
+        if location in country_list:
 
-        if location in NoDataExceptions:
-            print("No data available for:", location)
+            column = COUNTRY_REGION
+
+        elif location in province_list:
+
+            column = PROVINCE_STATE
+
+        elif location in Countries:
+
+            print("Country:", location)
+            country = Countries.get(location)
+            location = country.name
+            alternatives = [ country.alpha2, country.alpha3 ]   # there is also: numeric, apolitical
+
+            if location in CountryExceptions.keys():
+                location = CountryExceptions[location]
+
+            if location not in country_list:
+                print("No data available for:", location, "May need to add to CountryExceptions.")
+                return None, None, None
+
+            column = COUNTRY_REGION
+
+        elif location in LocationExceptions.keys():
+
+            print("LocationException:", location)
+            location = LocationExceptions[location]
+            column = COUNTRY_REGION
+
+        else:
+
+            print("Unknown location:", location, ". Please add to <LocationExceptions>.")
+            print("Abort")
             return None, None, None
 
-        if location in CountryExceptions.keys():
-            alternatives.insert(0, location)
-            location = CountryExceptions[location]
+        # now finally get the data for the location!
 
-        confirmed_data = pd.read_csv(DataLocation2 + ConfirmedFile)
-        deaths_data = pd.read_csv(DataLocation2 + DeathsFile)
-
-        rows = confirmed_data['Country/Region'] == location
-        row = list(rows[rows == True].index)
+        matching_rows = global_cases[column] == location
+        row = list(matching_rows[matching_rows == True].index)
         if len(row) == 0:
             print("Please add", location, "to <CountryExceptions>. for looking up the data coreectly.")
             # print("Please choose from: ", confirmed_data['Country/Region'].tolist())
             return None, None, None
 
-        if len(row) > 1:
-            states = [str(state) for state in confirmed_data['Province/State'][row].tolist() if not isinstance(state, float)]
+        if len(row) > 1 and column == COUNTRY_REGION:
+            states = [str(state) for state in global_cases[PROVINCE_STATE][row].tolist() if not isinstance(state, float)]
             print(location, "includes:", ', '.join(states))
 
-        confirmed = np.array(confirmed_data.iloc[row, 4:].sum().tolist())
-        cumul_confirmed = np.transpose(confirmed[1:])
-        daily_confirmed = np.transpose(confirmed[1:] - confirmed[:-1])
+        cases = np.array(global_cases.iloc[row, 4:].sum().tolist())
+        cumul_cases = np.transpose(cases[1:])
+        daily_cases = np.transpose(cases[1:] - cases[:-1])
 
-        deaths = np.array(deaths_data.iloc[row, 4:].sum().tolist())
+        deaths = np.array(global_deaths.iloc[row, 4:].sum().tolist())
         cumul_deaths = np.transpose(deaths[1:])
         daily_deaths = np.transpose(deaths[1:] - deaths[:-1])
 
-        xvalues = [x for x in range(len(daily_confirmed))]
-        days = deaths_data.loc[row[0]][4:].index.tolist()
+        xvalues = [x for x in range(len(daily_cases))]
+        days = global_deaths.loc[row[0]][4:].index.tolist()
         lastday = "" if len(days) == 0 else days[-1]
         # lastday = deaths_data.loc[row[0]][4:].index.tolist()[-1]
 
-    else:
-
-        print("Unknown location:", location, ". Please add to <LocationExceptions>.")
-        print("Abort")
-
-        return None, None, None
+    # by now we have: location, alternatives, daily_cases, daily_deaths, xvalues, cumul_deaths, cumul_cases
 
     parameters[LOCATION] = location
-    alternatives = '(' + ', '.join(alternatives) + ')'
+    alternatives = '(' + ', '.join(alternatives) + ')' if len(alternatives) > 0 else ''
 
     if True:
         print("for", location, alternatives, "as of", lastday)
         print("latest daily DEATHS:", daily_deaths[-1], "total DEATHS:", cumul_deaths[-1])
-        print("latest daily CASES:", daily_confirmed[-1], "total CASES:", cumul_confirmed[-1])
+        print("latest daily CASES:", daily_cases[-1], "total CASES:", cumul_cases[-1])
 
-    return daily_confirmed, daily_deaths, xvalues
+    return daily_cases, daily_deaths, xvalues
 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -182,6 +217,11 @@ def get_data(parameters):
 def plot_data(cases, deaths, xvalues, parameters):
     """
     """
+    location = parameters[LOCATION]
+
+    if location in PlotExceptions.keys():
+        location = PlotExceptions[location]
+
     font = {'family': 'serif', 'color': 'darkred', 'weight': 'normal', 'size': 16 }
     font = {'family': 'serif', 'color': 'darkred', 'weight': 'normal' }
 
@@ -199,11 +239,11 @@ def plot_data(cases, deaths, xvalues, parameters):
     ax.legend(title='Where:')
     plt.ylabel('Number of Cases', fontdict=font)
     plt.xlabel('Days Since Jan 21st', fontdict=font)
-    plt.title(parameters[LOCATION] + ' COVID-19 Cases', fontdict=font)
+    plt.title(location + ' COVID-19 Cases', fontdict=font)
     plt.subplots_adjust(left=0.15)
 
     if parameters[PDF]:
-        fig.savefig(parameters[LOCATION] + '-covid.pdf')
+        fig.savefig(location + '-covid.pdf')
 
     plt.show()   # display plot on screen
 
